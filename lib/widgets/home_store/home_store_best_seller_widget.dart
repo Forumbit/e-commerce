1bef54bade3269b9d8661af08720dacbe89cf3c0
Amir Kharisov <58177045+Forumbit@users.noreply.github.com>
2023-06-13 12:59:21 +0300

    ref: fix grid height with shrinkwrap

--- lib/widgets/home_store/home_store_best_seller_widget.dart
+++ lib/widgets/home_store/home_store_best_seller_widget.dart
@@ -6,81 +6,78 @@ class BestSellerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 850,
-      child: GridView.count(
-        padding: const EdgeInsets.only(top: 25),
-        crossAxisCount: 2,
-        physics: const NeverScrollableScrollPhysics(),
-        childAspectRatio: 181 / 227,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(76, 95, 143, 0.1),
-                  blurRadius: 20,
-                  offset: Offset(0, -5),
-                ),
-              ],
-            ),
-            child:
-                const _BestSellerItemWidget(assetName: Images.favoriteFill),
+    return GridView.count(
+      shrinkWrap: true,
+      padding: const EdgeInsets.only(top: 25),
+      crossAxisCount: 2,
+      physics: const NeverScrollableScrollPhysics(),
+      childAspectRatio: 181 / 227,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(76, 95, 143, 0.1),
+                blurRadius: 20,
+                offset: Offset(0, -5),
+              ),
+            ],
           ),
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(76, 95, 143, 0.1),
-                  blurRadius: 20,
-                  offset: Offset(0, -5),
-                ),
-              ],
-            ),
-            child:
-                const _BestSellerItemWidget(assetName: Images.favoriteOutline),
+          child: const _BestSellerItemWidget(assetName: Images.favoriteFill),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(76, 95, 143, 0.1),
+                blurRadius: 20,
+                offset: Offset(0, -5),
+              ),
+            ],
           ),
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(76, 95, 143, 0.1),
-                  blurRadius: 20,
-                  offset: Offset(0, -5),
-                ),
-              ],
-            ),
-            child: const _BestSellerItemWidget(assetName: Images.favoriteFill),
+          child:
+              const _BestSellerItemWidget(assetName: Images.favoriteOutline),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(76, 95, 143, 0.1),
+                blurRadius: 20,
+                offset: Offset(0, -5),
+              ),
+            ],
           ),
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(76, 95, 143, 0.1),
-                  blurRadius: 20,
-                  offset: Offset(0, -5),
-                ),
-              ],
-            ),
-            child:
-                const _BestSellerItemWidget(assetName: Images.favoriteOutline),
+          child: const _BestSellerItemWidget(assetName: Images.favoriteFill),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(76, 95, 143, 0.1),
+                blurRadius: 20,
+                offset: Offset(0, -5),
+              ),
+            ],
           ),
-          Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(76, 95, 143, 0.1),
-                  blurRadius: 20,
-                  offset: Offset(0, -5),
-                ),
-              ],
-            ),
-            child: const _BestSellerItemWidget(assetName: Images.favoriteFill),
+          child:
+              const _BestSellerItemWidget(assetName: Images.favoriteOutline),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(76, 95, 143, 0.1),
+                blurRadius: 20,
+                offset: Offset(0, -5),
+              ),
+            ],
           ),
-        ],
-      ),
+          child: const _BestSellerItemWidget(assetName: Images.favoriteFill),
+        ),
+      ],
     );
   }
 }
@@ -99,50 +96,56 @@ class _BestSellerItemWidget extends StatelessWidget {
       borderRadius: BorderRadius.circular(15),
       child: Stack(
         children: [
-          Container(
-            decoration: const BoxDecoration(color: Colors.white),
-            padding: const EdgeInsets.all(8),
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Image.network(
-                        'https://shop.gadgetufa.ru/images/upload/52534-smartfon-samsung-galaxy-s20-ultra-12-128-chernyj_1024.jpg'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 21),
-                    child: Row(
-                      children: const [
-                        Text(
-                          '\$1,047',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 16),
+          InkWell(
+            onTap: () {},
+            child: Ink(
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(8),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Image.network(
+                            'https://shop.gadgetufa.ru/images/upload/52534-smartfon-samsung-galaxy-s20-ultra-12-128-chernyj_1024.jpg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 21),
+                        child: Row(
+                          children: const [
+                            Text(
+                              '\$1,047',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 16),
+                            ),
+                            SizedBox(width: 7),
+                            Text(
+                              '\$1,500',
+                              style: TextStyle(
+                                decoration: TextDecoration.lineThrough,
+                                color: Color.fromRGBO(204, 204, 204, 1),
+                                fontSize: 10,
+                              ),
+                            )
+                          ],
                         ),
-                        SizedBox(width: 7),
-                        Text(
-                          '\$1,500',
-                          style: TextStyle(
-                            decoration: TextDecoration.lineThrough,
-                            color: Color.fromRGBO(204, 204, 204, 1),
-                            fontSize: 10,
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 21),
+                        child: Text(
+                          'Samsung Galaxy s20 Ultra',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      )
+                    ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 21),
-                    child: Text(
-                      'Samsung Galaxy s20 Ultra',
-                      style: TextStyle(fontSize: 10),
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
           ),
+          Container(),
           _FavoriteBestSellerWidgetButton(
             assetName: assetName,
           ),
